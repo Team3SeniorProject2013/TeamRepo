ae9ff909a6978d01c8d70fe2636af43b7b1d48d0
Shalan <Shalan@Shalan-PC>
2013-12-08 14:55:12 -0600

    evertthing

--- Extra/src/ReadingFiles.java
+++ Extra/src/ReadingFiles.java
@@ -22,20 +22,20 @@ import java.util.Date;
  */
 public class ReadingFiles 
 {
-	 static SimpleDateFormat dateFormat = new SimpleDateFormat("MM/DD/YYYY"); 
-	 
-	 
-	 
-	 private static Connection connect = null;
-	 private static Statement statement = null;
-	 private static PreparedStatement preparedStatement = null;
-	 private static ResultSet resultSet = null;  
-	 
-	 
+         static SimpleDateFormat dateFormat = new SimpleDateFormat("MM/DD/YYYY"); 
+         
+         
+         
+         private static Connection connect = null;
+         private static Statement statement = null;
+         private static PreparedStatement preparedStatement = null;
+         private static ResultSet resultSet = null;  
+         
+         
     public static void main(String[] args) throws Exception 
     {
-    	dbConnection();
-    	String selectSQL = "select FileName, Directory, FileSize, DateModified from file";
+            dbConnection();
+            String selectSQL = "select FileName, Directory, FileSize, DateModified from file";
         preparedStatement = connect.prepareStatement(selectSQL);
         //preparedStatement.setString(1, TagName);
         
@@ -43,10 +43,10 @@ public class ReadingFiles
         ArrayList arrayList = new ArrayList();
         
         while(resultSet.next()) {
-        	System.out.println(resultSet.getString("filename"));
-        	System.out.println(resultSet.getString("Directory"));
-        	System.out.println(resultSet.getString("FileSize"));
-        	System.out.println(resultSet.getString("DateModified"));
+                System.out.println(resultSet.getString("filename"));
+                System.out.println(resultSet.getString("Directory"));
+                System.out.println(resultSet.getString("FileSize"));
+                System.out.println(resultSet.getString("DateModified"));
 
             
         }
@@ -55,44 +55,44 @@ public class ReadingFiles
     }
     
     public static void dbConnection() throws Exception{
-    	Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
         connect = DriverManager
             .getConnection("jdbc:mysql://localhost/research knowledge manager?"
                 + "user=root&password=");
         
-    	
+            
     }
     
     
     
     public static void readfiles(String root) throws Exception{
-    	dbConnection();
-	   
+            dbConnection();
+           
         Scanner input = new Scanner(System.in);
      
    
         //asdfasdfasdf test push
         
-    	//System.out.println("Please enter root path:");
-    	//String root = input.nextLine();
+            //System.out.println("Please enter root path:");
+            //String root = input.nextLine();
        /**
-	    *	Example valid root path inputs: 
-	    *	1. C:\Users\Quang\Documents\Dropbox\9) Fall 2013\Software Engineering Project\testFiles
-	    *   2. C:\Users\Quang\Google Drive\Data Alliance Test Files\TESTDOCS\-- Bri Library --
+            *        Example valid root path inputs: 
+            *        1. C:\Users\Quang\Documents\Dropbox\9) Fall 2013\Software Engineering Project\testFiles
+            *   2. C:\Users\Quang\Google Drive\Data Alliance Test Files\TESTDOCS\-- Bri Library --
        **/
-    	
-    	//String root = "a";
-    	//String root = "C:\\Users\\Shalan\\Desktop\\TESTDOCS";
-    	
-    	searchFile(root);
-    	//searchFile2 (root);
-    	//searchFile3(root);
+            
+            //String root = "a";
+            //String root = "C:\\Users\\Shalan\\Desktop\\TESTDOCS";
+            
+            searchFile(root);
+            //searchFile2 (root);
+            //searchFile3(root);
     
     }
     
     public static void searchFile (String root) throws SQLException // input path and displays file names
     {
-	  File folder = new File(root); //creates new root file
+          File folder = new File(root); //creates new root file
       File[] listOfFiles = folder.listFiles();
       String subRoot = "";
   
@@ -109,11 +109,11 @@ public class ReadingFiles
       {
           if (listOfFiles[i].isFile()) 
           {
-              System.out.println("File:\t" + listOfFiles[i].getName()); 						//displays name of file
-              System.out.println("Path:\t" + listOfFiles[i].getPath());						//displays file path
-              //System.out.println("Parent:\t" + listOfFiles[i].getParentFile());				//displays directory path
-              //System.out.println("Parent:\t" + listOfFiles[i].getParentFile().getName());		// display parent file name
-              System.out.println("Size:\t"+listOfFiles[i].length());						//display file size
+              System.out.println("File:\t" + listOfFiles[i].getName());                                                 //displays name of file
+              System.out.println("Path:\t" + listOfFiles[i].getPath());                                                //displays file path
+              //System.out.println("Parent:\t" + listOfFiles[i].getParentFile());                                //displays directory path
+              //System.out.println("Parent:\t" + listOfFiles[i].getParentFile().getName());                // display parent file name
+              System.out.println("Size:\t"+listOfFiles[i].length());                                                //display file size
               System.out.println("Date Modified:\t"+ dateFormat.format(listOfFiles[i].lastModified())); //display date modified
               System.out.println(" ");
               
@@ -146,93 +146,18 @@ public class ReadingFiles
           else if (listOfFiles[i].isDirectory()) 
           {
               subRoot = listOfFiles[i].getName();
-        	  System.out.println("Subdirectory:\t" + subRoot);
+                  System.out.println("Subdirectory:\t" + subRoot);
               searchFile (root + "\\"+subRoot); //if it is a directory, recursively search sub directory
               
           }
       }
           
-    }	
-    /*
-    
-    public static void searchFile2 (String root)
-    {
-    	File folder = new File(root);
-    	File[] listOfFiles = folder.listFiles();
-    	String subRoot2 = "";
-    	
-    	String [] Directory = new String [listOfFiles.length];
-    	int DirectoryNum = 1;
-    	Directory[0] = listOfFiles[0].getParentFile().getName();
-    	
-    	for (int i = 0; i<listOfFiles.length; i++) // fines all directories and store them in array
-    	{
-    		if (listOfFiles[i].isDirectory())
-    		{
-    			Directory[DirectoryNum] = listOfFiles[i].getName();
-    			System.out.println(listOfFiles[i].getName());
-    			DirectoryNum++;
-    			subRoot2 = listOfFiles[i].getName();
-           	  	//System.out.println("Subdirectory:\t" + subRoot2);
-                searchFile2 (root + "\\"+subRoot2); //if it is a directory, recursively search sub directory
-    			
-    		}
-    		
-    	}
-    	
-    	for (int k = 0;k<DirectoryNum;k++)
-    	{
-    		System.out.println("Directory: " +Directory[k]);
-    		
-    		for (int l = 0; l <listOfFiles.length; l++) 
-    		{
-    			if (listOfFiles[l].isFile() && listOfFiles[l].getParentFile().getName() == Directory[k])
-    			{
-    				System.out.println(listOfFiles[l].getName());
-    			}
-    		}
-    	}
-    	
-    }
-    
+    }        
 
-    public static void searchFile3 (String root) throws SQLException // input path and displays file names
-    {
-	  File folder = new File(root); //creates new root file
-      File[] listOfFiles = folder.listFiles();
-      String subRoot = "";
-      
-      String [] Directory = new String [listOfFiles.length];
-      int DirectoryNum = 0;
-      
-      for (int i = 0; i < listOfFiles.length; i++) 
-      {
-          if (listOfFiles[i].isFile()) 
-          {
-              System.out.println("File:\t" + listOfFiles[i].getName()); 						//displays name of file
-              //System.out.println("Path:\t" + listOfFiles[i].getPath());						//displays file path
-              //System.out.println("Parent:\t" + listOfFiles[i].getParentFile());				//displays directory path
-              System.out.println("Parent:\t" + listOfFiles[i].getParentFile().getName());		// display parent file name
-              System.out.println("Size:\t"+listOfFiles[i].length()+"kb");						//display file size
-              //System.out.println("Date Modified:\t"+ dateFormat.format(listOfFiles[i].lastModified())); //display date modified
-              System.out.println(" ");
-
-          } 
-          else if (listOfFiles[i].isDirectory()) 
-          {
-              subRoot = listOfFiles[i].getName();
-        	  System.out.println("Subdirectory:\t" + subRoot);
-              searchFile (root + "\\"+subRoot); //if it is a directory, recursively search sub directory
-              
-          }
-      }
-          
-    }	
-    */
     
     public static ArrayList returnTagRearch(String TagName) throws Exception {
-    	
-    	dbConnection();
+            
+            dbConnection();
 
         //select FileName, Directory, FileSize, DateModified from file, tag where tag.FileID = file.FileIndex and tag = 'chicken'
 /*
@@ -249,34 +174,15 @@ public class ReadingFiles
         ArrayList arrayList = new ArrayList();
         
         while(resultSet.next()) {
-        	arrayList.add(resultSet.getString("filename"));
-        	arrayList.add(resultSet.getString("Directory"));
-        	arrayList.add(resultSet.getString("FileSize"));
-        	arrayList.add(resultSet.getString("DateModified"));
+                arrayList.add(resultSet.getString("filename"));
+                arrayList.add(resultSet.getString("Directory"));
+                arrayList.add(resultSet.getString("FileSize"));
+                arrayList.add(resultSet.getString("DateModified"));
 
             
         }
-    	
-    	return arrayList;
-    	
-    }
-    
-    
+            
+            return arrayList;
+            
+    }    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
